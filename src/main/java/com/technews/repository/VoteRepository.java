40b9e8193951c67056bcac5c639d55325b0341e5
zachary <zacharyberdell@gmail.com>
2022-04-21 15:50:55 -0600

    Built the routes for users.

--- src/main/java/com/technews/repository/VoteRepository.java
+++ src/main/java/com/technews/repository/VoteRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
-    @Query("SELECT count(*) FROM Vote v WHERE v.postId = :id;")
+    @Query("SELECT count(*) FROM Vote v WHERE v.postId = :id")
     int countVotesByPostId(@Param("id") Integer id);
 }
