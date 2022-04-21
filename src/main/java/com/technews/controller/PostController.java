3c74ed8dc9b0fce75e22e95ca7974e6b26eb13fa
zachary <zacharyberdell@gmail.com>
2022-04-21 17:01:23 -0600

    Post routes have been finished.

--- src/main/java/com/technews/controller/PostController.java
+++ src/main/java/com/technews/controller/PostController.java
@@ -1,14 +1,16 @@
 package com.technews.controller;
 
 import com.technews.model.Post;
+import com.technews.model.User;
+import com.technews.model.Vote;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -37,7 +39,51 @@ public class PostController {
     public Post getPost(@PathVariable Integer id) {
         Post returnPost = repository.getOne(id);
         returnPost.setVoteCount(VoteRepository.countVotesByPostId(returnPost.getId()));
-        
+
         return returnPost;
+    }
+
+    @PostMapping("/api/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post createPost(@RequestBody Post post) {
+        repository.save(post);
+
+        return post;
+    }
+
+    @PutMapping("/api/posts/{id}")
+    public Post updatePost(@PathVariable int id, @RequestBody Post post) {
+        Post tempPost = repository.getOne(id);
+        tempPost.setTitle(post.getTitle());
+
+        return repository.save(tempPost);
+    }
+
+    @PutMapping("/api/posts/upvote")
+    public String addVote(@RequestBody Vote vote, HttpServletRequest request) {
+        String returnValue = "";
+
+        if(request.getSession(false) != null) {
+            Post returnPost = null;
+
+            User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            vote.setUserId(sessionUser.getId());
+            VoteRepository.save(vote);
+
+            returnPost = repository.getOne(vote.getPostId());
+            returnPost.setVoteCount(VoteRepository.countVotesByPostId(vote.getPostId()));
+
+            returnValue = "";
+        } else {
+            returnValue = "login first";
+        }
+
+        return returnValue;
+    }
+
+    @DeleteMapping("/api/posts/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePost(@PathVariable int id) {
+        repository.deleteById(id);
     }
 }
